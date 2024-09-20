3ae08ab2151f693f5898c2269c2f7982eaba3e8f
Jefferson Duarte <duartejefferson123@gmail.com>
2024-09-20 14:36:23 -0300

    feat: create status enum

--- src/main/java/com/jefferson/helpdesk/domain/enums/STATUS.java
+++ src/main/java/com/jefferson/helpdesk/domain/enums/STATUS.java
@@ -1,14 +1,14 @@
 package com.jefferson.helpdesk.domain.enums;
 
-public enum Profile {
-    ADMIN(0, "ROLE_ADMIN"),
-    CLIENT(1, "ROLE_CLIENT"),
-    TECHNICAL(2, "ROLE_TECHNICAL");
+public enum STATUS {
+    OPEN(0, "OPEN"),
+    PROGRESS(1, "PROGRESS"),
+    CLOSED(2, "CLOSED");
 
     private Integer code;
     private String description;
 
-    Profile(Integer code, String description) {
+    STATUS(Integer code, String description) {
         this.code = code;
         this.description = description;
     }
@@ -29,17 +29,17 @@ public enum Profile {
         this.description = description;
     }
 
-    public static Profile toEnum(Integer code) {
+    public static STATUS toEnum(Integer code) {
         if (code == null) {
             return null;
         }
 
-        for (Profile p : Profile.values()) {
+        for (STATUS p : STATUS.values()) {
             if (code.equals(p.getCode())) {
                 return p;
             }
         }
-        throw new IllegalArgumentException("Invalid profile");
+        throw new IllegalArgumentException("Invalid status");
     }
 
 }
