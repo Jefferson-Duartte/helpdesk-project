42e6ec42fe97446729d54d70da29199f77040dc3
Jefferson Duarte <duartejefferson123@gmail.com>
2024-09-20 14:36:01 -0300

    feat: create profile enum

--- src/main/java/com/jefferson/helpdesk/domain/enums/PROFILE.java
+++ src/main/java/com/jefferson/helpdesk/domain/enums/PROFILE.java
@@ -1,4 +1,45 @@
 package com.jefferson.helpdesk.domain.enums;
 
-public class Profile {
+public enum PROFILE {
+    ADMIN(0, "ROLE_ADMIN"),
+    CLIENT(1, "ROLE_CLIENT"),
+    TECHNICAL(2, "ROLE_TECHNICAL");
+
+    private Integer code;
+    private String description;
+
+    PROFILE(Integer code, String description) {
+        this.code = code;
+        this.description = description;
+    }
+
+    public Integer getCode() {
+        return code;
+    }
+
+    public void setCode(Integer code) {
+        this.code = code;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public static PROFILE toEnum(Integer code) {
+        if (code == null) {
+            return null;
+        }
+
+        for (PROFILE p : PROFILE.values()) {
+            if (code.equals(p.getCode())) {
+                return p;
+            }
+        }
+        throw new IllegalArgumentException("Invalid profile");
+    }
+
 }
