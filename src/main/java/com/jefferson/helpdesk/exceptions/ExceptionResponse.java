282479a6e34d89d6cd125ead9076adb90bec9b5c
Jefferson Duarte <duartejefferson123@gmail.com>
2024-09-24 16:15:37 -0300

    feat: create ExceptionResponse

---
+++ src/main/java/com/jefferson/helpdesk/exceptions/ExceptionResponse.java
@@ -0,0 +1,17 @@
+package com.jefferson.helpdesk.exceptions;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class ExceptionResponse {
+    private LocalDateTime timestamp;
+    private String error;
+    private Integer status;
+    private String path;
+}
