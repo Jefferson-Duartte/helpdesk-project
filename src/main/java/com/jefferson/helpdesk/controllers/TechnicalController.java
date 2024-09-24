14effc9a94186ae765803b4e6583593fa7c765f0
Jefferson Duarte <duartejefferson123@gmail.com>
2024-09-24 16:16:13 -0300

    feat: create TechnicalController

---
+++ src/main/java/com/jefferson/helpdesk/controllers/TechnicalController.java
@@ -0,0 +1,26 @@
+package com.jefferson.helpdesk.controllers;
+
+import com.jefferson.helpdesk.domain.Technical;
+import com.jefferson.helpdesk.services.TechnicalService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/technicals")
+public class TechnicalController {
+
+    private TechnicalService service;
+
+    public TechnicalController(TechnicalService service) {
+        this.service = service;
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Technical> findById(@PathVariable Integer id){
+        return ResponseEntity.ok().body(service.findById(id));
+    }
+
+}
