ee036badf1ec8250d3efdce5b545893705c48516
Jefferson Duarte <duartejefferson123@gmail.com>
2024-09-24 16:15:47 -0300

    feat: create GlobalExceptionHandler

---
+++ src/main/java/com/jefferson/helpdesk/exceptions/handlers/GlobalExceptionHandler.java
@@ -0,0 +1,28 @@
+package com.jefferson.helpdesk.exceptions.handlers;
+
+import com.jefferson.helpdesk.exceptions.ExceptionResponse;
+import com.jefferson.helpdesk.exceptions.ObjectNotFoundException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.LocalDateTime;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    private ResponseEntity<ExceptionResponse> buildResponseEntity(Exception exception, HttpStatus status, HttpServletRequest request){
+
+        ExceptionResponse response = new ExceptionResponse(LocalDateTime.now(), exception.getMessage(), status.value(), request.getRequestURI());
+        return ResponseEntity.status(status).body(response);
+    }
+
+    @ExceptionHandler(ObjectNotFoundException.class)
+    public ResponseEntity<ExceptionResponse> objectNotFound(ObjectNotFoundException exception, HttpServletRequest request){
+        return buildResponseEntity(exception, HttpStatus.NOT_FOUND, request);
+    }
+
+}
