5030c7dc3d47904aea64e19d086ad056e32aa7dd
Jefferson Duarte <duartejefferson123@gmail.com>
2024-09-27 01:11:54 -0300

    feat: add ExceptionResponse class

--- src/main/java/com/jefferson/helpdesk/exceptions/ExceptionResponse.java
+++ src/main/java/com/jefferson/helpdesk/exceptions/ExceptionResponse.java
@@ -2,6 +2,7 @@ package com.jefferson.helpdesk.exceptions;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
@@ -9,6 +10,7 @@ import java.time.LocalDateTime;
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 public class ExceptionResponse {
     private LocalDateTime timestamp;
     private String error;
