c7861c88744a163810d30ca4ab2e63d544387fd2
Jefferson Duarte <duartejefferson123@gmail.com>
2024-10-09 10:27:56 -0300

    feat: add annotations in ClientResponseDTO

--- src/main/java/com/jefferson/helpdesk/domain/dtos/client/ClientResponseDTO.java
+++ src/main/java/com/jefferson/helpdesk/domain/dtos/client/ClientResponseDTO.java
@@ -1,10 +1,18 @@
 package com.jefferson.helpdesk.domain.dtos.client;
 
 import com.jefferson.helpdesk.domain.enums.PROFILE;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.time.LocalDate;
 import java.util.Set;
 
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class ClientResponseDTO {
     private Integer id;
     private String name;
