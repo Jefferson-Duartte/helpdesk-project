b08a59ac794fdce24078b4a5de673f2780858923
Jefferson Duarte <duartejefferson123@gmail.com>
2024-10-10 15:03:28 -0300

    feat: implements save and checkIfUserExists methods in client service implementation

--- src/main/java/com/jefferson/helpdesk/repositories/ClientRepository.java
+++ src/main/java/com/jefferson/helpdesk/repositories/ClientRepository.java
@@ -3,5 +3,9 @@ package com.jefferson.helpdesk.repositories;
 import com.jefferson.helpdesk.domain.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ClientRepository extends JpaRepository<Client, Integer> {
+    Optional<Client> findByEmail(String email);
+    Optional<Client> findByDocument(String document);
 }
--- src/main/java/com/jefferson/helpdesk/services/ClientServiceImpl.java
+++ src/main/java/com/jefferson/helpdesk/services/ClientServiceImpl.java
@@ -1,10 +1,15 @@
 package com.jefferson.helpdesk.services;
 
+import com.jefferson.helpdesk.domain.Client;
+import com.jefferson.helpdesk.domain.dtos.client.ClientRequestDTO;
 import com.jefferson.helpdesk.domain.dtos.client.ClientResponseDTO;
+import com.jefferson.helpdesk.exceptions.DataIntegrityViolationException;
 import com.jefferson.helpdesk.exceptions.ObjectNotFoundException;
 import com.jefferson.helpdesk.mappers.ClientMapper;
 import com.jefferson.helpdesk.repositories.ClientRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class ClientServiceImpl implements IClientService{
@@ -21,5 +26,26 @@ public class ClientServiceImpl implements IClientService{
     public ClientResponseDTO findById(Integer id) {
         var client = repository.findById(id).orElseThrow(() -> new ObjectNotFoundException("Usuário não encontrado!"));
         return mapper.toResponseDTO(client);
+    }
+
+    @Override
+    public ClientResponseDTO save(ClientRequestDTO dto) {
+        checkIfUserExists(dto);
+        return mapper.toResponseDTO(repository.save(mapper.toEntity(dto)));
+    }
+
+    public void checkIfUserExists(ClientRequestDTO dto) {
+        Optional<Client> emailObj = repository.findByEmail(dto.getEmail());
+        Optional<Client> documentObj = repository.findByDocument(dto.getDocument());
+
+        if (emailObj.isPresent() && !emailObj.get().getDocument().equals(dto.getDocument()) ||
+                documentObj.isPresent() && !documentObj.get().getEmail().equals(dto.getEmail())) {
+            throw new DataIntegrityViolationException("Dados inválidos");
+        }
+        if (emailObj.isPresent() && documentObj.isPresent() &&
+                emailObj.get().getEmail().equals(dto.getEmail()) &&
+                documentObj.get().getDocument().equals(dto.getDocument())) {
+            throw new DataIntegrityViolationException("Usuário já existe");
+        }
     }
 }
