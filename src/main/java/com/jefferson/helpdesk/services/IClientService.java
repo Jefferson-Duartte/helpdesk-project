7027d30d7addc3b134f9679dbd88131f4dd715ed
Jefferson Duarte <duartejefferson123@gmail.com>
2024-10-10 15:03:00 -0300

    feat: create save and checkIfUserExists methods in client service interface

--- src/main/java/com/jefferson/helpdesk/services/IClientService.java
+++ src/main/java/com/jefferson/helpdesk/services/IClientService.java
@@ -1,8 +1,14 @@
 package com.jefferson.helpdesk.services;
 
+import com.jefferson.helpdesk.domain.dtos.client.ClientRequestDTO;
 import com.jefferson.helpdesk.domain.dtos.client.ClientResponseDTO;
 
 public interface IClientService {
 
     ClientResponseDTO findById(Integer id);
+
+    ClientResponseDTO save(ClientRequestDTO dto);
+
+    void checkIfUserExists(ClientRequestDTO dto);
+
 }
