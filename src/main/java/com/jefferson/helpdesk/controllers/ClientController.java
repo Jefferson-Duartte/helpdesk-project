5604b66e5bb2e8b6fc414c38b74b329ea7492027
Jefferson Duarte <duartejefferson123@gmail.com>
2024-10-13 23:56:00 -0300

    feat: implements Client update endpoint

--- src/main/java/com/jefferson/helpdesk/controllers/ClientController.java
+++ src/main/java/com/jefferson/helpdesk/controllers/ClientController.java
@@ -3,6 +3,7 @@ package com.jefferson.helpdesk.controllers;
 import com.jefferson.helpdesk.domain.dtos.client.ClientRequestDTO;
 import com.jefferson.helpdesk.domain.dtos.client.ClientResponseDTO;
 import com.jefferson.helpdesk.services.ClientServiceImpl;
+import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -25,10 +26,15 @@ public class ClientController {
     }
 
     @PostMapping
-    public ResponseEntity<ClientResponseDTO> save(@RequestBody ClientRequestDTO dto){
+    public ResponseEntity<ClientResponseDTO> save(@RequestBody @Valid ClientRequestDTO dto){
         var client = service.save(dto);
         URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{id}").buildAndExpand(client.getId()).toUri();
         return ResponseEntity.created(uri).body(client);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ClientResponseDTO> update(@PathVariable Integer id, @RequestBody @Valid ClientRequestDTO dto){
+        return ResponseEntity.ok().body(service.update(id, dto));
     }
 
 }
