cf1453c88365723ce191c739ce4060f1515b8743
Jefferson Duarte <duartejefferson123@gmail.com>
2024-10-13 23:47:39 -0300

    feat: add profiles attribute

--- src/main/java/com/jefferson/helpdesk/domain/dtos/client/ClientRequestDTO.java
+++ src/main/java/com/jefferson/helpdesk/domain/dtos/client/ClientRequestDTO.java
@@ -1,11 +1,14 @@
 package com.jefferson.helpdesk.domain.dtos.client;
 
+import com.jefferson.helpdesk.domain.enums.PROFILE;
 import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.hibernate.validator.constraints.br.CPF;
+
+import java.util.Set;
 
 @Getter
 @Setter
@@ -25,5 +28,7 @@ public class ClientRequestDTO {
 
     @NotEmpty
     private String password;
+
+    private Set<PROFILE> profiles;
 
 }
