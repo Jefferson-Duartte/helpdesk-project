55d807c2ed6cb77ded0373b9e4e770c93c471da9
Jefferson Duarte <duartejefferson123@gmail.com>
2024-10-21 11:34:15 -0300

    feat: adds validations in fields

--- src/main/java/com/jefferson/helpdesk/domain/dtos/ticket/TicketRequestDTO.java
+++ src/main/java/com/jefferson/helpdesk/domain/dtos/ticket/TicketRequestDTO.java
@@ -1,5 +1,7 @@
 package com.jefferson.helpdesk.domain.dtos.ticket;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,13 +12,25 @@ import lombok.Setter;
 @AllArgsConstructor
 @NoArgsConstructor
 public class TicketRequestDTO {
+
     private Integer id;
+
+    @NotNull(message = "O campo PRIORITY é requerido")
     private Integer priority;
+
+    @NotNull(message = "O campo TECHNICAL é requerido")
     private Integer technical;
+
+    @NotNull(message = "O campo CLIENT é requerido")
     private Integer client;
+
+    @NotNull(message = "O campo STATUS é requerido")
     private Integer status;
+
+    @NotEmpty(message = "O campo TITLE é requerido")
     private String title;
+
+    @NotEmpty(message = "O campo OBSERVATIONS é requerido")
     private String observations;
-    private String clientName;
-    private String technicalName;
+
 }
