9ad87d121c0b0f3a832926794ea0e5326b9f5107
Jefferson Duarte <duartejefferson123@gmail.com>
2024-10-21 10:59:05 -0300

    feat: implements find all endpoint

--- src/main/java/com/jefferson/helpdesk/controllers/TicketController.java
+++ src/main/java/com/jefferson/helpdesk/controllers/TicketController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/tickets")
 public class TicketController {
@@ -22,5 +24,11 @@ public class TicketController {
     public ResponseEntity<TicketResponseDTO> findById(@PathVariable Integer id){
         return  ResponseEntity.ok().body(service.findById(id));
     }
+
+    @GetMapping
+    public ResponseEntity<List<TicketResponseDTO>> findAll(){
+        return ResponseEntity.ok().body(service.findAll());
+    }
+
 
 }
