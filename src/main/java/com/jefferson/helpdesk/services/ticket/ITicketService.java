e766a34c0b3462c35347506e4b93c2fbf5b20879
Jefferson Duarte <duartejefferson123@gmail.com>
2024-10-21 10:57:32 -0300

    feat: create find all method

--- src/main/java/com/jefferson/helpdesk/services/ticket/ITicketService.java
+++ src/main/java/com/jefferson/helpdesk/services/ticket/ITicketService.java
@@ -1,11 +1,17 @@
 package com.jefferson.helpdesk.services.ticket;
 
 import com.jefferson.helpdesk.domain.Ticket;
+import com.jefferson.helpdesk.domain.dtos.technical.TechnicalResponseDTO;
 import com.jefferson.helpdesk.domain.dtos.ticket.TicketResponseDTO;
+
+import java.util.List;
 
 public interface ITicketService {
     TicketResponseDTO findById(Integer id);
 
     Ticket findByIdEntity(Integer id);
+
+    List<TicketResponseDTO> findAll();
+
 
 }
