cd64e3c365f8469aa47a0e64ca69eaa95ddfc882
Jefferson Duarte <duartejefferson123@gmail.com>
2024-10-21 01:45:27 -0300

    feat: implements findById endpoint

---
+++ src/main/java/com/jefferson/helpdesk/controllers/TicketController.java
@@ -0,0 +1,26 @@
+package com.jefferson.helpdesk.controllers;
+
+import com.jefferson.helpdesk.domain.dtos.ticket.TicketResponseDTO;
+import com.jefferson.helpdesk.services.ticket.TicketServiceImpl;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/tickets")
+public class TicketController {
+
+    private final TicketServiceImpl service;
+
+    public TicketController(TicketServiceImpl service) {
+        this.service = service;
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<TicketResponseDTO> findById(@PathVariable Integer id){
+        return  ResponseEntity.ok().body(service.findById(id));
+    }
+
+}
