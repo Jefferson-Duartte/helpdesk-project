3d841aed0bb9735b50e1b5c1a1445d3a56b26a75
Jefferson Duarte <duartejefferson123@gmail.com>
2024-10-21 01:44:48 -0300

    feat: add @Mapping annotation

--- src/main/java/com/jefferson/helpdesk/mappers/TicketMapper.java
+++ src/main/java/com/jefferson/helpdesk/mappers/TicketMapper.java
@@ -4,14 +4,19 @@ import com.jefferson.helpdesk.domain.Ticket;
 import com.jefferson.helpdesk.domain.dtos.ticket.TicketRequestDTO;
 import com.jefferson.helpdesk.domain.dtos.ticket.TicketResponseDTO;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface TicketMapper {
 
+    @Mapping(target = "technical", source = "technical", ignore = true)
+    @Mapping(target = "client", source = "client", ignore = true)
     Ticket toEntity(TicketRequestDTO dto);
 
+    @Mapping(target = "technical", source = "technical", ignore = true)
+    @Mapping(target = "client", source = "client", ignore = true)
     TicketResponseDTO toResponseDTO(Ticket obj);
 
     List<TicketResponseDTO> toListResponseDTO(List<Ticket> objList);
