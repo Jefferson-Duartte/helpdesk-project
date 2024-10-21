f4f68bcce75942776715a1658e0f8a77608c119f
Jefferson Duarte <duartejefferson123@gmail.com>
2024-10-21 10:55:00 -0300

    feat: mapping fields

--- src/main/java/com/jefferson/helpdesk/mappers/TicketMapper.java
+++ src/main/java/com/jefferson/helpdesk/mappers/TicketMapper.java
@@ -11,12 +11,14 @@ import java.util.List;
 @Mapper(componentModel = "spring")
 public interface TicketMapper {
 
-    @Mapping(target = "technical", source = "technical", ignore = true)
-    @Mapping(target = "client", source = "client", ignore = true)
+    @Mapping(target = "technical.id", source = "technical")
+    @Mapping(target = "client.id", source = "client")
     Ticket toEntity(TicketRequestDTO dto);
 
-    @Mapping(target = "technical", source = "technical", ignore = true)
-    @Mapping(target = "client", source = "client", ignore = true)
+    @Mapping(target = "technical", source = "technical.id")
+    @Mapping(target = "technicalName", source = "technical.name")
+    @Mapping(target = "client", source = "client.id")
+    @Mapping(target = "clientName", source = "client.name")
     TicketResponseDTO toResponseDTO(Ticket obj);
 
     List<TicketResponseDTO> toListResponseDTO(List<Ticket> objList);
